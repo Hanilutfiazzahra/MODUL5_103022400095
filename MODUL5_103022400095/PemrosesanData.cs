10cf152400369a52e67e0808072810da352473a7
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2026-04-01 07:22:19 +0700

    menambahkan class PemrosesanData dengan method DapatkanNilaiTerbesar

---
+++ MODUL5_103022400095/PemrosesanData.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MODUL5_103022400095
+{
+    internal class PemrosesanData
+    {
+        public void DapatkanNilaiTerpesar<T> (T a, T b, T c) 
+        {
+            dynamic maks;
+            dynamic temp1 = a;
+            dynamic temp2 = b;
+            dynamic temp3 = c;
+
+            if (temp1 > temp2 && temp1 > temp2)
+            {
+                maks = a;
+            }
+            else if (temp2 > temp1 && temp2 > temp3)
+            {
+                maks = b;
+            }
+            else
+            {
+                maks = c;
+            }
+
+            Console.WriteLine("Nilai terbesar adalah: " + maks);
+
+        }
+    }
+}
--- MODUL5_103022400095/Program.cs
+++ MODUL5_103022400095/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("MODUL 5 KONSTRUKSI PERANGKAT LUNAK");
+using MODUL5_103022400095;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("MODUL 5 KONTRUKSI PERANGKAT LUNAK");
+
+        PemrosesanData data = new PemrosesanData();
+        data.DapatkanNilaiTerpesar(10, 30, 22);
+    }
+
+        
+ }
+
+
+
+
